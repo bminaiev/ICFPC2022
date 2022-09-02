72db4af91e930a8b24f48b9467607d25aeeb5932
Roman Udovichenko <mailromka@gmail.com>
2022-09-02 15:37:01 +0300

    txt inputs and converter

---
+++ conv.py
@@ -0,0 +1,16 @@
+from PIL import Image
+import os
+
+image_dir = "images/"
+inputs_dir = "inputs/"
+for f in os.listdir(image_dir):
+    fname = image_dir + f
+    img = Image.open(fname)
+    w, h = img.size
+    with open(inputs_dir + f.replace("png", "txt"), "w") as fout:
+        fout.write(f"{h} {w}\n")
+        for y in range(h):
+            for x in range(w):
+                fout.write(" ".join(map(str, img.getpixel((x, y)))) + " ")
+            fout.write("\n")
+
