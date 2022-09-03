337d21bd15d02315df52023910c10f49c073ef6d
Roman Udovichenko <mailromka@gmail.com>
2022-09-03 16:30:59 +0300

    Inputs with blocks

--- conv.py
+++ conv.py
@@ -1,13 +1,17 @@
 from PIL import Image
-import os
+import os, json
 
 image_dir = "images/"
 inputs_dir = "inputs/"
-for f in sorted(os.listdir(image_dir)):
+jsons_dir = "initial/"
+for f in sorted(os.listdir(image_dir), key=lambda s: int(s[:s.find('.')])):
     fname = image_dir + f
     print(fname + "...")
     img = Image.open(fname)
     w, h = img.size
+    js = json.load(open(jsons_dir + f.replace("png", "json")))
+    assert(w == js['width'])
+    assert(h == js['height'])
     with open(inputs_dir + f.replace("png", "txt"), "w") as fout:
         fout.write(f"{h} {w}\n")
         for y in range(h):
@@ -15,3 +19,7 @@ for f in sorted(os.listdir(image_dir)):
                 fout.write(" ".join(map(str, img.getpixel((x, y)))) + " ")
             fout.write("\n")
 
+        fout.write(f"{len(js['blocks'])}\n")
+        for i, b in enumerate(js['blocks']):
+            assert(int(b['blockId']) == i)
+            fout.write(f"{b['blockId']} " + " ".join(map(str, b['bottomLeft'] + b["topRight"] + b["color"])) + "\n")
